7743a50eeaf7665cae9a8f633ed4461c14cfbdb1
md-abu-kayser <abu.kayser.official@gmail.com>
2026-09-02 00:17:07 +0600

    learn: create CommonJS utility aggregator module

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,12 @@
+// Import utility functions
 const { add } = require("./add");
 const { subtract } = require("./subtract");
 
+// Export utilities from a single module
 module.exports = {
   add,
   subtract,
 };
+
+console.log("Addition:", add(20, 10));
+console.log("Subtraction:", subtract(20, 10));
